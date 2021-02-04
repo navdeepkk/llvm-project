d26f5b0f97c31e6eb13e9cb35618793e62a15896
Navdeep Kumar <navdeep.navdeep37@gmail.com>
2021-02-04 02:04:37 -0500

    [MLIR][CUDA-RUNNER] Add CL option to pass SM version and index-bitwidth Add a CL option to pass sm version in MLIR CUDA Runner. There are currently no checks on the compatibility/validity of PTX and SM version. This is just naive addition to check the working of WMMA tensor core operations. Add CL option to pass index-bitwidth for LLVM lowering passes on the device side.

--- mlir/tools/mlir-cuda-runner/mlir-cuda-runner.cpp
+++ mlir/tools/mlir-cuda-runner/mlir-cuda-runner.cpp
@@ -115,6 +115,15 @@ struct GpuToCubinPipelineOptions
       llvm::cl::desc("Annotation attribute string for GPU binary")};
 };
 
+static llvm::cl::opt<std::string>
+    clSMVersion("sm", llvm::cl::desc("SM version to target"),
+                llvm::cl::init("sm_35"));
+
+static llvm::cl::opt<unsigned>
+    clIndexWidth("index-bitwidth",
+                 llvm::cl::desc("Bitwidth of index type to use for lowering"),
+                 llvm::cl::init(32));
+
 // Register cuda-runner specific passes.
 static void registerCudaRunnerPasses() {
   PassPipelineRegistration<GpuToCubinPipelineOptions> registerGpuToCubin(
@@ -123,10 +132,10 @@ static void registerCudaRunnerPasses() {
         pm.addPass(createGpuKernelOutliningPass());
         auto &kernelPm = pm.nest<gpu::GPUModuleOp>();
         kernelPm.addPass(createStripDebugInfoPass());
-        kernelPm.addPass(createLowerGpuOpsToNVVMOpsPass());
-        kernelPm.addPass(createConvertGPUKernelToBlobPass(
-            translateModuleToLLVMIR, compilePtxToCubin, "nvptx64-nvidia-cuda",
-            "sm_35", "+ptx60", options.gpuBinaryAnnotation));
+        kernelPm.addPass(createLowerGpuOpsToNVVMOpsPass(clIndexWidth.getValue()));
+	kernelPm.addPass(createConvertGPUKernelToBlobPass(
+	    translateModuleToLLVMIR, compilePtxToCubin, "nvptx64-nvidia-cuda",
+	    clSMVersion.getValue(), "+ptx60", options.gpuBinaryAnnotation));
       });
   registerGPUPasses();
   registerGpuToLLVMConversionPassPass();
